38692c4a25ce37347c81d28a666ab47210f14d2a
Carlos Ortega Morales <caomboy19@gmail.com>
2017-09-19 17:04:53 -0500

    Add project files.

---
+++ ContosoUniversity/Models/ContosoUniversityContext.cs
@@ -0,0 +1,29 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace ContosoUniversity.Models
+{
+    public class ContosoUniversityContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public ContosoUniversityContext() : base("name=ContosoUniversityContext")
+        {
+
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Course> Courses { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
